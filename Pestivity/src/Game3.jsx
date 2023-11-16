5b2d59f97e8d23d28a88857a52e033e9cbff948a
PsychedelicOrangePeel <raghudhruv2@gmail.com>
2023-11-16 13:00:52 +0530

    Changes to rules

--- Pestivity/src/Game3.jsx
+++ Pestivity/src/Game3.jsx
@@ -3,7 +3,7 @@ import { Hang } from "./Hangman"
 export let GameHang = (props) => {
   return (
     <div ref={props.scroll} style={{height : '100dvh'}} className=" grid grid-rows-5 md:grid-cols-5 md:grid-rows-1 border-t border-b rounded-sm border-gray-700">
-      <GameR rules='Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque voluptatum sit saepe minima porro quas ipsam, consequatur a veritatis minus.'/>
+      <GameR rules='Hangman is a simple game where you have to guess the word by entering the letters one at a time. You have 6 chances to guess the word. Everytime you guess it wrong, a part of a picture where a man is being hung is drawn. Dont let the computer complete the picture! '/>
       <div className=' row-span-4 col-auto md:col-span-4 '>
         <Hang/>
       </div>
--- Pestivity/src/Landing.jsx
+++ Pestivity/src/Landing.jsx
@@ -2,7 +2,7 @@ export let LandingHero = () => {
   return (
     <div className=" flex justify-center items-center flex-col md:gap-20 gap-5">
       <div className=" w-10/12 space-y-5">
-        <p className=" text-7xl md:text-9xl">P<span className=" italic text-blue-400">es</span>tivity</p>
+        <p className=" text-7xl md:text-9xl italic text">P<span className=" italic text-blue-400">ES</span>tivity</p>
         <p className=" text-3xl md:text-5xl">A Mini Games Web Application</p>
       </div>
     </div>
