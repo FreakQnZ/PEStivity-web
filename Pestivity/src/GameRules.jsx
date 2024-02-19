758ae1ab1c1dc40f32912d2ef7a29e283490ad71
Aryan <100415551+arugoat@users.noreply.github.com>
2024-02-19 13:03:04 +0530

    Update GameRules.jsx

--- Pestivity/src/GameRules.jsx
+++ Pestivity/src/GameRules.jsx
@@ -12,7 +12,7 @@ export let GameR = (props) => {
       <div className='hidden md:block'>
         <a href="https://github.com/FreakQnZ/PEStivity-web" target='_blank' className='flex gap-4 items-center hover:text-white'>
           <img src={git} width={30} alt="" />
-          <p className='text-lg'>Visit the Repo</p>
+          <p className='text-lg'>Visit The Repository</p>
         </a>
       </div>
     </div>
